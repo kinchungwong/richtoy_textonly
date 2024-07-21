59f38c6f8124480840e6f0e32cfa3f36c57cb471
Ryan Wong <kinchungwong@users.noreply.github.com>
2024-07-21 16:41:37 -0700

    WIP: int_int_multimap_test
    New unit test design decouples initialization data from assertions, making them orthogonal.

--- test0/int_int_multimap_test.py
+++ test0/int_int_multimap_test.py
@@ -1,22 +1,16 @@
+import builtins
 from collections.abc import Iterable
-import typing
+from dataclasses import dataclass
+import inspect
+import sys
+from typing import ForwardRef
 import unittest
 
 
 from src0.collections.int_int_multimap import IntIntMultimap
 
-class IntIntMultimapEmptyTest(unittest.TestCase):
-    def test_smoke(self):
-        _ = IntIntMultimap()
 
-    def test_len_is_zero(self):
-        obj = IntIntMultimap()
-        self.assertEqual(len(obj), 0)
-
-    def test_iter_expect_noexec(self):
-        obj = IntIntMultimap()
-        for _ in obj:
-            self.fail("When empty, iterating on obj.__iter__() should not have received values.")
+IntIntMultimapTest = ForwardRef("IntIntMultimapTest")
 
     def test_items_expect_noexec(self):
         obj = IntIntMultimap()
@@ -25,372 +19,87 @@ class IntIntMultimapEmptyTest(unittest.TestCase):
 
     def test_contains_key(self):
         obj = IntIntMultimap()
-        for k in range(-10, 11):
-            self.assertFalse(k in obj, "When empty, __contains__(key) should return false.")
-    
-    def test_contains_item(self):
-        obj = IntIntMultimap()
-        for k in range(-10, 11):
-            for v in range(-10, 11):
-                self.assertFalse(obj.contains_item(k, v), "When empty, contains_item(key, value) should return false.")
-
-    def test_clear_smoke_1(self):
-        obj = IntIntMultimap()
-        obj.clear()
-
-    def test_copy_smoke_1(self):
-        obj = IntIntMultimap()
-        _ = obj.copy()
-
-
-class IntIntMultimapSingleItemReadTest(unittest.TestCase):
-
-    def setUp(self):
-        self.key_one = 1
-        self.value_two = 2
-
-    def create_object(self) -> IntIntMultimap:
-        obj = IntIntMultimap()
-        obj.add(self.key_one, self.value_two)
-        return obj
-
-    def test_smoke(self):
-        obj = self.create_object()
-
-    def test_len_is_one(self):
-        obj = self.create_object()
-        self.assertEqual(len(obj), 1)
-
-    def test_iter_expect_one(self):
-        obj = self.create_object()
-        for key in obj:
-            self.assertEqual(key, self.key_one)
-
-    def test_items_expect_one(self):
-        obj = self.create_object()
-        for key, value in obj.items():
-            self.assertEqual((key, value), (self.key_one, self.value_two))
-
-    def test_contains_key(self):
-        obj = self.create_object()
-        self.assertTrue(self.key_one in obj)
-
-    def test_contains_key_not(self):
-        obj = self.create_object()
-        self.assertFalse(self.value_two in obj)
-
-    def test_contains_item(self):
-        obj = self.create_object()
-        self.assertTrue(obj.contains_item(self.key_one, self.value_two))
-
-    def test_contains_item_not(self):
-        obj = self.create_object()
-        self.assertFalse(obj.contains_item(self.value_two, self.key_one))
-
-    def test_clear_smoke_1(self):
-        obj = self.create_object()
-        obj.clear()
-
-    def test_clear_len_is_zero(self):
-        obj = self.create_object()
-        obj.clear()
-        self.assertEqual(len(obj), 0)
-
-    def test_copy_smoke_1(self):
-        obj = self.create_object()
-        copied = obj.copy()
-
-    def test_copy_len_is_one(self):
-        obj = self.create_object()
-        copied = obj.copy()
-        for key, value in copied.items():
-            self.assertEqual((key, value), (self.key_one, self.value_two))
-
-
-class IntIntMultimapSingleItemIdempotentTest(unittest.TestCase):
-
-    def setUp(self):
-        self.key_one = 1
-        self.value_two = 2
-
-    def create_object(self) -> IntIntMultimap:
-        obj = IntIntMultimap()
-        obj.add(self.key_one, self.value_two)
-        obj.add(self.key_one, self.value_two)
-        return obj
-
-    def test_smoke(self):
-        obj = self.create_object()
-
-    def test_len_is_one(self):
-        obj = self.create_object()
-        self.assertEqual(len(obj), 1)
-
-    def test_iter_expect_one(self):
-        obj = self.create_object()
-        iter_count = 0
+        assertions.assertEqual(obj.total, 0)
         for key in obj.keys():
-            self.assertEqual(key, self.key_one)
-            iter_count += 1
-        self.assertEqual(iter_count, 1)
-
-    def test_items_expect_one(self):
-        obj = self.create_object()
-        iter_count = 0
+            assertions.fail()
         for key, value in obj.items():
-            self.assertEqual((key, value), (self.key_one, self.value_two))
-            iter_count += 1
-        self.assertEqual(iter_count, 1)
-
-    def test_contains_key(self):
-        obj = self.create_object()
-        self.assertTrue(self.key_one in obj)
-
-    def test_contains_key_not(self):
-        obj = self.create_object()
-        self.assertFalse(self.value_two in obj)
-
-    def test_contains_item(self):
-        obj = self.create_object()
-        self.assertTrue(obj.contains_item(self.key_one, self.value_two))
-
-    def test_contains_item_not(self):
-        obj = self.create_object()
-        self.assertFalse(obj.contains_item(self.value_two, self.key_one))
-
-    def test_clear_smoke_1(self):
-        obj = self.create_object()
-        obj.clear()
-
-    def test_clear_len_is_zero(self):
-        obj = self.create_object()
-        obj.clear()
-        self.assertEqual(len(obj), 0)
-
-    def test_copy_smoke_1(self):
-        obj = self.create_object()
-        copied = obj.copy()
-
-    def test_copy_len_is_one(self):
-        obj = self.create_object()
-        copied = obj.copy()
-        iter_count = 0
-        for key, value in copied.items():
-            self.assertEqual((key, value), (self.key_one, self.value_two))
-            iter_count += 1
-        self.assertEqual(iter_count, 1)
-
-class IntIntMultimapMultiKeyReadTest(unittest.TestCase):
-
-    def setUp(self):
-        self._kv_1_2 = (1, 2)
-        self._kv_3_4 = (3, 4)
+            assertions.fail()
     
-    def create_object(self) -> IntIntMultimap:
+    @staticmethod
+    def test_add_smoke(assertions: unittest.TestCase, data: _TestData):
+        trace_print(inspect.currentframe().f_code.co_name)
         obj = IntIntMultimap()
-        obj.add(*self._kv_1_2)
-        obj.add(*self._kv_3_4)
-        return obj
+        for key, value in data._items:
+            obj.add(key, value)
 
-    def test_smoke(self):
-        obj = self.create_object()
+    @staticmethod
+    def test_total(assertions: unittest.TestCase, data: _TestData):
+        trace_print(inspect.currentframe().f_code.co_name)
+        obj = IntIntMultimap()
+        for key, value in data._items:
+            obj.add(key, value)
+        assertions.assertEqual(obj.total, len(data._items))
 
-    def test_len_is_two(self):
-        obj = self.create_object()
-        self.assertEqual(len(obj), 2)
+    @staticmethod
+    def test_keys(assertions: unittest.TestCase, data: _TestData):
+        trace_print(inspect.currentframe().f_code.co_name)
+        obj = IntIntMultimap()
+        for key, value in data._items:
+            obj.add(key, value)
+        keys_from_obj = obj.keys()
+        for key in keys_from_obj:
+            assertions.assertIn(key, data._keys)
+            assertions.assertNotIn(key, data._notkeys)
+        for key in data._keys:
+            assertions.assertIn(key, keys_from_obj)
+        for notkey in data._notkeys:
+            assertions.assertNotIn(notkey, keys_from_obj)
 
-    def test_iter_expect_two(self):
-        obj = self.create_object()
-        expected_keyset = set([
-            self._kv_1_2[0],
-            self._kv_3_4[0],
-        ])
-        iter_count = 0
-        for key in obj.keys():
-            self.assertIn(key, expected_keyset)
-            iter_count += 1
-        self.assertEqual(iter_count, 2)
-
-    def test_items_expect_one(self):
-        obj = self.create_object()
-        expected_itemset = set([
-            self._kv_1_2,
-            self._kv_3_4,
-        ])
-        iter_count = 0
+    @staticmethod
+    def test_clear(assertions: unittest.TestCase, data: _TestData):
+        trace_print(inspect.currentframe().f_code.co_name)
+        obj = IntIntMultimap()
+        for key, value in data._items:
+            obj.add(key, value)
+        obj.clear()
+        assertions.assertEqual(obj.total, 0)
+        keys_from_obj = obj.keys()
+        assertions.assertEqual(len(keys_from_obj), 0)
+        for key in keys_from_obj:
+            assertions.fail()
         for key, value in obj.items():
-            self.assertIn((key, value), expected_itemset)
-            iter_count += 1
-        self.assertEqual(iter_count, 2)
+            pass
+        
 
-    def test_contains_key(self):
-        obj = self.create_object()
-        expected_keyset = set([
-            self._kv_1_2[0],
-            self._kv_3_4[0],
-        ])
-        for expected_key in expected_keyset:
-            self.assertIn(expected_key, obj)
+class IntIntMultimapTest(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-    def test_contains_key_not(self):
-        obj = self.create_object()
-        not_expected_keyset = set([
-            self._kv_1_2[1],
-            self._kv_3_4[1],
-        ])
-        for not_expected_key in not_expected_keyset:
-            self.assertNotIn(not_expected_key, obj)
-
-    def test_contains_item(self):
-        obj = self.create_object()
-        expected_itemset = set([
-            self._kv_1_2,
-            self._kv_3_4,
-        ])
-        for key, value in expected_itemset:
-            self.assertTrue(obj.contains_item(key, value))
-
-    def test_contains_item_not(self):
-        obj = self.create_object()
-        not_expected_itemset = set([
-            tuple(reversed(self._kv_1_2)),
-            tuple(reversed(self._kv_3_4)),
-        ])
-        for not_key, not_value in not_expected_itemset:
-            self.assertFalse(obj.contains_item(not_key, not_value))
-
-    def test_clear_smoke_1(self):
-        obj = self.create_object()
-        obj.clear()
-
-    def test_clear_len_is_zero(self):
-        obj = self.create_object()
-        obj.clear()
-        self.assertEqual(len(obj), 0)
-
-    def test_copy_smoke_1(self):
-        obj = self.create_object()
-        copied = obj.copy()
-
-    def test_copy_len_is_two(self):
-        obj = self.create_object()
-        copied = obj.copy()
-        expected_itemset = set([
-            self._kv_1_2,
-            self._kv_3_4,
-        ])
-        iter_count = 0
-        for key, value in copied.items():
-            self.assertIn((key, value), expected_itemset)
-            iter_count += 1
-        self.assertEqual(iter_count, 2)
-
-
-# class IntIntMultimapMultiValueReadTest(unittest.TestCase):
-
-#     def setUp(self):
-#         self._kv_1_2 = (1, 2)
-#         self._kv_1_4 = (1, 4)
-    
-#     def create_object(self) -> IntIntMultimap:
-#         obj = IntIntMultimap()
-#         obj.add(*self._kv_1_2)
-#         obj.add(*self._kv_1_4)
-#         return obj
-
-#     def test_smoke(self):
-#         obj = self.create_object()
-
-#     def test_len_is_two(self):
-#         obj = self.create_object()
-#         self.assertEqual(len(obj), 2)
-
-#     def test_iter_expect_one(self):
-#         obj = self.create_object()
-#         expected_keyset = set([
-#             self._kv_1_2[0],
-#             self._kv_1_4[0],
-#         ])
-#         iter_count = 0
-#         for key in obj:
-#             self.assertIn(key, expected_keyset)
-#             iter_count += 1
-#         self.assertEqual(iter_count, 1)
-
-#     def test_items_expect_one(self):
-#         obj = self.create_object()
-#         expected_itemset = set([
-#             self._kv_1_2,
-#             self._kv_3_4,
-#         ])
-#         iter_count = 0
-#         for key, value in obj.items():
-#             self.assertIn((key, value), expected_itemset)
-#             iter_count += 1
-#         self.assertEqual(iter_count, 2)
-
-#     def test_contains_key(self):
-#         obj = self.create_object()
-#         expected_keyset = set([
-#             self._kv_1_2[0],
-#             self._kv_3_4[0],
-#         ])
-#         for expected_key in expected_keyset:
-#             self.assertIn(expected_key, obj)
-
-#     def test_contains_key_not(self):
-#         obj = self.create_object()
-#         not_expected_keyset = set([
-#             self._kv_1_2[1],
-#             self._kv_3_4[1],
-#         ])
-#         for not_expected_key in not_expected_keyset:
-#             self.assertNotIn(not_expected_key, obj)
-
-#     def test_contains_item(self):
-#         obj = self.create_object()
-#         expected_itemset = set([
-#             self._kv_1_2,
-#             self._kv_3_4,
-#         ])
-#         for key, value in expected_itemset:
-#             self.assertTrue(obj.contains_item(key, value))
-
-#     def test_contains_item_not(self):
-#         obj = self.create_object()
-#         not_expected_itemset = set([
-#             tuple(reversed(self._kv_1_2)),
-#             tuple(reversed(self._kv_3_4)),
-#         ])
-#         for not_key, not_value in not_expected_itemset:
-#             self.assertFalse(obj.contains_item(not_key, not_value))
-
-#     def test_clear_smoke_1(self):
-#         obj = self.create_object()
-#         obj.clear()
-
-#     def test_clear_len_is_zero(self):
-#         obj = self.create_object()
-#         obj.clear()
-#         self.assertEqual(len(obj), 0)
-
-#     def test_copy_smoke_1(self):
-#         obj = self.create_object()
-#         copied = obj.copy()
-
-#     def test_copy_len_is_two(self):
-#         obj = self.create_object()
-#         copied = obj.copy()
-#         expected_itemset = set([
-#             self._kv_1_2,
-#             self._kv_3_4,
-#         ])
-#         iter_count = 0
-#         for key, value in copied.items():
-#             self.assertIn((key, value), expected_itemset)
-#             iter_count += 1
-#         self.assertEqual(iter_count, 2)
+    def test_all(self):
+        trace_print(inspect.currentframe().f_code.co_name)
+        data_methods = [
+            TestData.empty,
+            TestData.single_item,
+            TestData.two_different_keys,
+            TestData.same_key_two_values,
+            TestData.two_symmetric,
+        ]
+        assert_methods = [
+            TestMethods.test_init,
+            TestMethods.test_add_smoke,
+            TestMethods.test_total,
+            TestMethods.test_keys,
+            TestMethods.test_clear,
+        ]
+        for data_idx, data_method in enumerate(data_methods):
+            for assert_idx, assert_method in enumerate(assert_methods):
+                data_name = f"[{data_idx}]{data_method.__name__}"
+                assert_name = f"[{assert_idx}]{assert_method.__name__}"
+                with self.subTest(",".join((data_name, assert_name))):
+                    data = data_method()
+                    assert_method(self, data)
 
 
 if __name__ == "__main__":
     unittest.main()
+   
